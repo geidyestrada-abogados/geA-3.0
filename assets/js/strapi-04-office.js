957892aa0869d0c86aace4b95b9b7fa80d4ce311
geidyestrada-abogados <torra@automaticaintegral.com>
2026-01-20 23:10:22 -0500

    Update strapi-04-office.js

--- assets/js/strapi-04-office.js
+++ assets/js/strapi-04-office.js
@@ -47,8 +47,8 @@ async function fetchOfficeContent() {
 
     // Establece la imagen de la oficina desde Strapi si existe
     if (officeData.Office && officeData.Office.url) {
-      const officeUrl = `https://gea-strapi.up.railway.app${officeData.Office.url}`;
-      const baseUrl = "https://gea-strapi.up.railway.app"; // Cambia esto si usas otra URL base para Strapi
+      const officeUrl = `https://automatic-cheese-6aca9a943b.strapiapp.com${officeData.Office.url}`;
+      const baseUrl = "https://automatic-cheese-6aca9a943b.strapiapp.com"; // Cambia esto si usas otra URL base para Strapi
       const largeImageUrl = `${baseUrl}${officeData.Office.formats.large.url}`; // Utiliza 'large' o el formato deseado
 
       console.log("URL de fondo desde Strapi:", officeUrl); // Verifica que esta URL sea correcta
@@ -77,7 +77,7 @@ document.addEventListener("DOMContentLoaded", fetchOfficeContent);
 //TODO -- INICIO FUNCIÓN -- loadOfficeContent() (Cargar contenido de OFFICE en el Editor) ///////////////////////////////////////////////////////////////////////////////////////////////
 async function loadOfficeContent() {
   try {
-    const response = await fetch("https://gea-strapi.up.railway.app/api/office?populate=*");
+    const response = await fetch("https://automatic-cheese-6aca9a943b.strapiapp.com/api/office?populate=*");
     if (!response.ok) {
       throw new Error(
         "Error al obtener el contenido del Office: " + response.status
@@ -106,7 +106,7 @@ async function loadOfficeContent() {
         // Completa la URL con el dominio de Strapi si es relativa
         const fullOfficeUrl = officeUrl.startsWith("http")
           ? officeUrl
-          : `https://gea-strapi.up.railway.app${officeUrl}`;
+          : `https://automatic-cheese-6aca9a943b.strapiapp.com${officeUrl}`;
 
         document.getElementById(
           "office"
@@ -145,7 +145,7 @@ let selectedOfficeId = null; // Para almacenar el ID de la imagen seleccionada
 async function fetchOfficeBackgrounds() {
   try {
     const response = await fetch(
-      "https://gea-strapi.up.railway.app/api/office?populate=Office"
+      "https://automatic-cheese-6aca9a943b.strapiapp.com/api/office?populate=Office"
     );
     const officeData = await response.json();
 
@@ -169,7 +169,7 @@ function renderOfficeImages(officeImages) {
 
   officeImages.forEach((image) => {
     const imgElement = document.createElement("img");
-    imgElement.src = `https://gea-strapi.up.railway.app${image.url}`;
+    imgElement.src = `https://automatic-cheese-6aca9a943b.strapiapp.com${image.url}`;
     imgElement.alt = image.name;
     imgElement.classList.add("thumbnail");
 
@@ -196,7 +196,7 @@ function renderOfficeImages(officeImages) {
 function updateOfficePreview() {
   const previewElement = document.getElementById("officePreview");
   if (previewElement && selectedOfficeUrl) {
-    previewElement.style.backgroundImage = `url(https://gea-strapi.up.railway.app${selectedOfficeUrl})`;
+    previewElement.style.backgroundImage = `url(https://automatic-cheese-6aca9a943b.strapiapp.com${selectedOfficeUrl})`;
     console.log("Vista previa actualizada con:", selectedOfficeUrl);
   }
 }
@@ -217,7 +217,7 @@ async function saveOffice() {
 
     // Obtiene el arreglo actual `Office` de Strapi
     const currentResponse = await fetch(
-      "https://gea-strapi.up.railway.app/api/office?populate=Office"
+      "https://automatic-cheese-6aca9a943b.strapiapp.com/api/office?populate=Office"
     );
     const currentData = await currentResponse.json();
     const existingOfficeImages = currentData.data.Office || [];
@@ -231,7 +231,7 @@ async function saveOffice() {
     ];
 
     // Envía el arreglo completo actualizado a Strapi
-    const response = await fetch("https://gea-strapi.up.railway.app/api/office", {
+    const response = await fetch("https://automatic-cheese-6aca9a943b.strapiapp.com/api/office", {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -248,7 +248,7 @@ async function saveOffice() {
     console.log("PUT response data:", responseData);
 
     const updatedResponse = await fetch(
-      "https://gea-strapi.up.railway.app/api/office?populate=Office"
+      "https://automatic-cheese-6aca9a943b.strapiapp.com/api/office?populate=Office"
     );
     const updatedData = await updatedResponse.json();
     console.log("Estado de Office tras actualizar:", updatedData.data.Office);
@@ -264,7 +264,7 @@ async function saveOffice() {
 function updateOfficeBackground() {
   const officeElement = document.getElementById("office");
   if (officeElement && selectedOfficeUrl) {
-    officeElement.style.backgroundImage = `url(https://gea-strapi.up.railway.app${selectedOfficeUrl})`;
+    officeElement.style.backgroundImage = `url(https://automatic-cheese-6aca9a943b.strapiapp.com${selectedOfficeUrl})`;
     console.log("Actualización en el Office con:", selectedOfficeUrl);
   }
 }
@@ -297,7 +297,7 @@ async function updateOfficeContent() {
   const newItem4 = document.getElementById("edit-item4").value;
 
   try {
-    const response = await fetch("https://gea-strapi.up.railway.app/api/office", {
+    const response = await fetch("https://automatic-cheese-6aca9a943b.strapiapp.com/api/office", {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -326,4 +326,5 @@ async function updateOfficeContent() {
   }
 }
 //TODO -- FIN DE FUNCIÓN --> updateOfficeContent() (Función para cargar y mostrar la primera imagen de la oficina en Office) ////////////////////////////////////////////////
+
 
